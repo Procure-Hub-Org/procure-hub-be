338fb7a3f53a157db4dde63ea7ac8a1494d00c7a
Mina Duranovic <119140559+minaduranovic@users.noreply.github.com>
2025-04-03 23:41:47 +0200

    Revert "User registration feature PR"

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const serverConfig = require('./src/config/server');
 const frontendConfig = require('./src/config/frontend');
-const userRoutes = require('./src/routes/userRoutes.js');
 
 const app = express();
 
 app.use(cors({
     origin: frontendConfig.frontendUrl
 }));
-
-app.use(express.json());
-
-app.use('/api', userRoutes);
 
 app.listen(serverConfig.port, () => {
     console.log(`Server is running on port ${serverConfig.port}`);
--- src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController.js');
-const { registerValidator } = require('../middleware/userValidator.js');
-
-router.post('/user/register', registerValidator, userController.register);
-
-module.exports = router;
