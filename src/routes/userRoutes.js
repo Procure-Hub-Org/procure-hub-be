387875c8971d79eae1b0a43a31fd338229639112
lhadzic1 <lhadzic1@etf.unsa.ba>
2025-04-05 12:24:18 +0200

    Api for profile setup #6

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+
 const userController = require('../controllers/userController.js');
+const profileController = require("../controllers/profileController");
+
+const authMiddleware = require("../middleware/authMiddleware"); // provjera ulogovanog korisnika 
+const upload = require("../middleware/uploadMiddleware"); // middleware za upload slika
 const { registerValidator } = require('../middleware/userValidator.js');
 
 router.post('/user/register', registerValidator, userController.register);
+router.put("/user/profile", authMiddleware, 
+    upload.fields([
+     {name: "profile_picture", maxCount: 1},
+     {name: "company_logo", maxCount: 1},
+    ]),
+    profileController.updateProfile
+ );
 
 module.exports = router;
