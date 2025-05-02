b2f0dcff4e241779153520618c787da911b13653
amila <akukic1@etf.unsa.ba>
2025-05-02 13:33:28 +0200

    Quick fixes and testing socket auction updates

--- src/routes/auctionRoutes.js
+++ src/routes/auctionRoutes.js
@@ -4,24 +4,4 @@ const auctionController = require('../controllers/auctionController');
 
 router.get('/auction/:id', auctionController.getLiveAuction);
 
-
-
-router.get('/test/socket/:id', async (req, res) => {
-    try {
-        const { getLiveAuctionData } = require('../services/auctionInformationService');
-        const { getIO } = require('../config/socket');
-        const io = getIO();
-
-        const auctionId = req.params.id;
-        const data = await getLiveAuctionData(auctionId);
-
-        // Emituj podatke svim klijentima u toj sobi
-        io.to(`auction-${auctionId}`).emit('auctionData', data);
-
-        res.status(200).json({ message: 'Emitovano', data });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-});
-
 module.exports = router;
