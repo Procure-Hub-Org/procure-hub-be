aaa0475d2c593a026dbb5ef9d1a5eaf8274ee697
molovcic <molovcic1@etf.unsa.ba>
2025-05-30 18:30:27 +0200

    Revert "get contract change requests added"
    This reverts commit f0fc8f109d844dc03e67f02d26aa9109a47278b5.

--- src/routes/contractChangeRequestRoute.js
+++
@@ -1,8 +0,0 @@
-const { getContractChangeRequests } = require('../controllers/contractChangeRequestsController');
-const express = require('express');
-const { verifyToken } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.get('/requested-changes/:contractId', verifyToken, getContractChangeRequests);
-
-module.exports = router;
